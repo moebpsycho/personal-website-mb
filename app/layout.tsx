f2d98a3f042ffdc4739999fb6e79a2577a6044f9
Mya Berlanga <moebpsycho@gmail.com>
2024-10-30 00:59:28 -0500

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ramiro Santos",
+  title: "Mya Berlanga",
   description: "Ramiro Santos Website",
 };
 
